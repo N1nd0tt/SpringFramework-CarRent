c094f108c17e499b67adf04c4f7cb467ca0060fc
N1nd0tt <oleksiy.lopatynskiy@gmail.com>
2025-05-24 21:29:15 +0200

    z08 dodane zad dla chetnych

--- src/main/java/com/umcsuser/car_rent/controller/UserController.java
+++ src/main/java/com/umcsuser/car_rent/controller/UserController.java
@@ -4,6 +4,7 @@ import com.umcsuser.car_rent.models.User;
 import com.umcsuser.car_rent.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,12 +21,23 @@ public class UserController {
     }
 
     @DeleteMapping("/admin/{id}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<String> softDeleteUser(@PathVariable String id) {
         userService.softDeleteUser(id);
         return ResponseEntity.ok("User soft-deleted successfully.");
     }
 
+    @GetMapping("/me")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<User> getLoggedInUserInfo(Authentication authentication) {
+        String username = authentication.getName();
+        User user = userService.findByLogin(username)
+                .orElseThrow(() -> new IllegalArgumentException("User not found."));
+        return ResponseEntity.ok(user);
+    }
+
     @GetMapping
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> getUserInfo(Authentication authentication) {
         boolean isAdmin = authentication.getAuthorities().stream()
                 .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"));
@@ -42,12 +54,14 @@ public class UserController {
     }
 
     @PutMapping("/admin/{id}/remove-role")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<String> removeRoleFromUser(@PathVariable String id, @RequestParam String role) {
         userService.removeRole(id, role);
         return ResponseEntity.ok("Role removed successfully.");
     }
 
     @PutMapping("/admin/{id}/add-role")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<String> addRoleToUser(@PathVariable String id, @RequestParam String role) {
         userService.addRole(id, role);
         return ResponseEntity.ok("Role added successfully.");
